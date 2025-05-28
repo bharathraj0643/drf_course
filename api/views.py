17222f0a0dfaee910ce4d2d60ac27be2fc8f2449
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-28 16:02:23 +0530

    nested serializers, SerializersMethodField 	modified:   api/models.py 	modified:   api/urls.py 	modified:   api/views.py 	modified:   drf/settings.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from api.serializers import ProductSerializer, OrderSerializer
+from api.models import Product, Order, OrderItem
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+
+
+@api_view(["GET"])
+def product_list(request):
+    products = Product.objects.all()
+    serializers = ProductSerializer(products, many=True)
+    return Response(serializers.data)
+
+
+@api_view(["GET"])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializers = ProductSerializer(product)
+    return Response(serializers.data)
+
+
+@api_view(["GET"])
+def order_list(request):
+    orders = Order.objects.all()
+    serializers = OrderSerializer(orders, many=True)
+    return Response(serializers.data)
--- drf/settings.py
+++ drf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     "django_extensions",
     "api",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
