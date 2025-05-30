65193a718ee7b87e8ed341e9814bf8b11350f88a
bharathraj0643 <bharathraj0643@gmail.com>
2025-05-30 13:03:32 +0530

    Updating and Deleting Data using RetrieveUpdateDestroyAPIView

--- api/views.py
+++ api/views.py
@@ -27,10 +27,16 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         return super().get_permissions()
 
 
-class ProductDetailView(generics.RetrieveAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg = "product_id"
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method in ["PUT","PATCH","DELETE"]:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 class OrderListAPIView(generics.ListAPIView):
