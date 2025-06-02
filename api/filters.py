cc8305e9043cfc7aa913feebe439af1bb6829ef2
Bharath Raj S <bharathraj0643@gmail.com>
2025-06-02 09:50:19 +0530

    viewset actions , filtering and permissions 	modified:   api/filters.py 	modified:   api/views.py 	modified:   requirements.txt => installed isort for formatting imports

--- api/filters.py
+++ api/filters.py
@@ -1,7 +1,8 @@
 import django_filters
-from api.models import Product
+from api.models import Product, Order
 
 from rest_framework import filters
+
 
 class InStockFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
@@ -15,3 +16,10 @@ class ProductFilter(django_filters.FilterSet):
             "name": ["iexact", "icontains"],
             "price": ["exact", "lt", "gt", "range"],
         }
+
+
+class OrderFilter(django_filters.FilterSet):
+    created_at=django_filters.DateFilter(field_name='created_at__date')
+    class Meta:
+        model = Order
+        fields = {"status": ["exact"], "created_at": ["lt", "gt", "exact"]}
